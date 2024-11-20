ab46d244533b41b8fb394edb87d743e059504e7b
Tiger <billath131908@gmail.com>
2024-11-20 16:48:40 +0200

    what now?

--- omp.c
+++ omp.c
@@ -1,50 +1,124 @@
+/*
+ *  === Αρχείο: omp.c ===
+ *
+ *  Ονοματεπώνυμο: Αθανασίου Βασίλειος Ευάγγελος
+ *  Αριθμός Μητρώου: 19390005
+ *  Πρόγραμμα Σπουδών: ΠΑΔΑ
+ * 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <omp.h>
 
-#define CZ 1
+#define CZ 2
 #define N 10
 // σσ2. Τον αριθμό των threads τον δίνει ο χρήστης
-#define T 5
+#define T 6
 
-void printArray(int Array[N][N]);
+void print2DArray(FILE *fp, int Array[N][N]);
 
 int main(int argc, char *argv[]) 
 {
-    int A[N][N];
-    int B[N][N];
-    int M[T];
-    FILE *fp;
-    int i, j;
-    int chunk, flag, tid;
-    int loc_sum, loc_flag, loc_index, loc_min;
-    int incr, temp0, temp1;
-    int m;
-    int min_val;
-    double all_time_start, all_time_end;
-    double loc_time_start, loc_time_end;
+    int A[N][N];                                // Ο πίνακας Α που θα εξετάσουμε αν είναι αυστηρά διαγώνια δεσπόζων
+    int B[N][N];                                // Bij = m – |Aij| για i<>j και Bij = m για i=j 
+    int M[T];                                   // Ο πίνακας Μ που θα χρησιμοποιήσουμε για τον αλγόριθμο δυαδικού δένδρου
+    FILE *fpA, *fpB;                            // Αρχεία εξόδου για την αποθήκευση των πινάκων Α και Β
+    int i, j;                                   // Δείκτες επανάληψης
+    int rowSum;                                 // Άθροισμα των στοιχείων μιας γραμμής του πίνακα Α
+    int chunk, flag, tid;                       // Αριθμός επαναλήψεων ανά thread, Είναι ή δεν είναι ο Α αυστηρά διαγώνια δεσπόζων, Αναγνωριστικό thread
+    int loc_sum, loc_flag, loc_index, loc_min;  // Τοπικές μεταβλητές για κάθε thread
+    int incr, temp0, temp1;                     // Μεταβλητές για τον αλγόριθμο δυαδικού δένδρου
+    int m;                                      // Μέγιστη τιμή της διαγωνίου του πίνακα Α
+    int min_val;                                // Ελάχιστη τιμή του πίνακα Β
+    double all_time_start, all_time_end;        // Χρόνος εκτέλεσης του παράλληλου προγράμματος
+    double loc_time_start, loc_time_end;        // Χρόνος εκτέλεσης της κάθε παράλληλης εργασίας
 
+/*
+ *  Ορισμός του αριθμού των threads και άνοιγμα των αρχείων εξόδου 
+ */
     omp_set_num_threads(T);
+
+    if (argc != 3) 
+    {
+        printf("Usage: %s A.txt B.txt\n", argv[0]);
+        exit(1);
+    }
+
+    fpA = fopen(argv[1], "w");
+    if (fpA == NULL) 
+    {
+        printf("Cannot open file %s\n", argv[1]);
+        exit(1);
+    }
+
+    fpB = fopen(argv[2], "w");
+    if (fpB == NULL) 
+    {
+        printf("Cannot open file %s\n", argv[2]);
+        exit(1);
+    }
+
+/*
+ *  Αρχικοποίησεις 
+ *  του αριθμού επαναλήψεων ανά thread, 
+ *  του flag που ελέγχει αν ο Α είναι αυστηρά διαγώνια δεσπόζων,
+ *  του πίνακα Α
+ */
+    srand(time(NULL));
 
     chunk = CZ;
     flag = 1;
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            if (i == j) 
-                A[i][j] = N + i + 1;
-            else
-                A[i][j] = (i + j) % 3;
+    if (rand() % 2) {
+        // Strictly Diagonal Dominant
+        for (i = 0; i < N; i++) {
+            rowSum = 0;
+            for (j = 0; j < N; j++) {
+                if (i == j) {
+                    A[i][j] = rand() % 21 - 10; // Diagonal element (-10 to 10)
+                    A[i][j] = A[i][j] >= 0 ? A[i][j] + 20 : A[i][j] - 20; // Ensure larger magnitude
+                } else {
+                    A[i][j] = rand() % 21 - 10; // Off-diagonal element (-10 to 10)
+                    rowSum += abs(A[i][j]);
+                }
+            }
+            // Ensure diagonal element is greater than sum of other row elements
+            if (abs(A[i][i]) <= rowSum) {
+                A[i][i] = rowSum + rand() % 5 + 1; // Adjust to maintain strict diagonal dominance
+                A[i][i] *= (rand() % 2 == 0) ? 1 : -1; // Randomly keep it positive or negative
+            }
+        }
+    } else {
+        // NOT Strictly Diagonal Dominant
+        for (i = 0; i < N; i++) {
+            rowSum = 0;
+            for (j = 0; j < N; j++) {
+                if (i == j) {
+                    A[i][j] = rand() % 11 - 5; // Diagonal element (-5 to 5)
+                } else {
+                    A[i][j] = rand() % 21 - 10; // Off-diagonal element (-10 to 10)
+                    rowSum += abs(A[i][j]);
+                }
+            }
+            // Ensure diagonal element is smaller than or equal to sum of other row elements
+            if (abs(A[i][i]) > rowSum) {
+                A[i][i] = rowSum - rand() % 5 - 1; // Adjust to violate strict diagonal dominance
+                A[i][i] *= (rand() % 2 == 0) ? 1 : -1; // Randomly keep it positive or negative
+            }
         }
     }
 
-    m = A[0][0];
+    print2DArray(fpA, A);
 
-    printArray(A);
-
+/*
+ *  Έναρξη μέτρησης χρόνου εκτέλεσης του παράλληλου προγράμματος
+ */
     all_time_start = omp_get_wtime();
     
-    // a. Να ελέγχει (παράλληλα) αν ο πίνακας Α είναι αυστηρά διαγώνια δεσπόζων 
+/*
+ *  a. Να ελέγχει (παράλληλα) αν ο πίνακας Α είναι αυστηρά διαγώνια δεσπόζων
+ */  
     loc_time_start = omp_get_wtime();   
     
     #pragma omp parallel shared(flag) private(i, j, loc_sum, loc_flag, loc_index)
@@ -85,6 +159,8 @@ int main(int argc, char *argv[])
     printf("Task a. finished in %lf \n", loc_time_end - loc_time_start);
 
     // b. m = max(|Aii|), i = 0...N-1
+    m = A[0][0];
+
     loc_time_start = omp_get_wtime();
 
     #pragma omp parallel default(shared) private(i)
@@ -118,7 +194,7 @@ int main(int argc, char *argv[])
     loc_time_end = omp_get_wtime();
     // --------- Parallel Finish ---------
 
-    printArray(B);
+    print2DArray(fpB, B);
     printf("Task c. finished in %lf \n", loc_time_end - loc_time_start);
     
     min_val = B[0][0];
@@ -229,18 +305,21 @@ int main(int argc, char *argv[])
     all_time_end = omp_get_wtime();
     printf("Parallel program finished in %lf \n", all_time_end - all_time_start);
 
+    fclose(fpA);
+    fclose(fpB);
+
     return 0;
 }
 
-void printArray(int Array[N][N])
+void print2DArray(FILE *fp, int Array[N][N])
 {
     int i, j;
 
     for (i = 0; i < N; i++)
     {
         for (j = 0; j < N; j++)
-            printf("%d ", Array[i][j]);
-        printf("\n");
+            fprintf(fp, "%4d ", Array[i][j]);
+        fprintf(fp, "\n");
     }
 }
 
