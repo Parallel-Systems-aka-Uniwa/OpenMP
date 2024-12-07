cdbbfc187ad574ed0f5055aa485d47db2f1b3bff
Tiger <billath131908@gmail.com>
2024-12-07 21:03:49 +0200

    OpenMP

---
+++ 2A/omp_msort.c
@@ -0,0 +1,26 @@
+/*
+ *  === Αρχείο: omp_msort.c ===
+ *
+ *  Ονοματεπώνυμο: Αθανασίου Βασίλειος Ευάγγελος
+ *  Αριθμός Μητρώου: 19390005
+ *  Πρόγραμμα Σπουδών: ΠΑΔΑ
+ *  
+ *  Μεταγλώττιση: gcc -o omp_msort omp_msort.c -fopenmp
+ *  Εκτέλεση: ./omp_msort A.txt
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+// σσ2. Τον αριθμό των threads τον δίνει ο χρήστης
+#define T 1
+#define N 10
+#define CZ 2
+
+int main(int argc, char *argv[]) 
+{
+    return 0;
+}
